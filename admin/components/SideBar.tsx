8f033ae5cede1b8ae861295c441b986889806d37
robdodson <robdodson@google.com>
2021-12-12 16:46:35 -0800

    add active state to nav

--- admin/components/SideBar.tsx
+++ admin/components/SideBar.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const SideBar = () => {
+  const router = useRouter();
   return (
     <div className="sidebar">
       <div className="sidebar__header">Rust multiplayer</div>
       <nav className="sidebar__nav">
-        <Link href="/">Connections</Link>
+        <Link href="/">
+          <a
+            className="sidebar__link"
+            data-active={router.pathname === '/' ? '' : null}
+          >
+            Connections
+          </a>
+        </Link>
       </nav>
     </div>
   );
